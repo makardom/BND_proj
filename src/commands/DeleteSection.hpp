7798cb63c27bd5bfed6b06b81b710d5d9ce88fe2
Maksim <maksimvrus371@gmail.com>
2023-06-13 01:06:57 +0300

    changes

--- src/commands/DeleteSection.hpp
+++ src/commands/DeleteSection.hpp
@@ -15,9 +15,6 @@ private:
     std::string setName(const keyArgs_t &keys);
     static constexpr char NOT_ENOUGH_ARGS[] = "Not enough arguments (see INIT --help).";
     static constexpr char TOO_MANY_ARGS[] = "Too many arguments (see INIT --help).";
-    static constexpr char LENGTH_CANT_CONVERT[] = "Length has incorrect format.";
-    static constexpr char LENGTH_RESTRICTED[] = "Length must be in range [1, 65535].";
-    static constexpr char NO_LENGTH_VALUE[] = "length can’t be empty.";
     static constexpr char NO_NAME_VALUE[] = "name can’t be empty.";
     static constexpr char NAME_TOO_LONG[] = "The name is too long (maximum 10 chars).";
     static constexpr char NAME_INCORRECT[] = "Name has incorrect format.";
