bac9ce1b089a65252dd2c278afabe2aac397bacb
Maksim <maksimvrus371@gmail.com>
2023-06-17 15:35:49 +0300

    documentation added

--- src/commands/BNDCommandFactory.hpp
+++ src/commands/BNDCommandFactory.hpp
@@ -1,21 +1,40 @@
 //
 // Created by maksi on 08.06.2023.
 //
-
+/**
+ * @file BNDCommandFactory.h
+ * @brief Определение класса шаблона BNDCommandFactory
+ */
 #ifndef MONITOR_MYCOMMANDFACTORY_HPP
 #define MONITOR_MYCOMMANDFACTORY_HPP
 
 #include "CommandFactory.hpp"
 #include "LibraryData.hpp"
 
+/**
+* @class BNDCommandFactory
+* @tparam CommandClasses Тип создаваемых команд
+* @brief Фабричный класс, создающий команды типа CommandClasses с использованием библиотеки данных.
+*/
 template<typename CommandClasses>
 class BNDCommandFactory : public CommandFactory<CommandClasses> {
 private:
-    LibraryData &libraryData;
+    LibraryData &libraryData;///< Ссылка на объект LibraryData
 
 public:
+
+    /**
+    * @brief Конструктор класса BNDCommandFactory
+    * @param libraryData Ссылка на объект LibraryData, который будет использоваться при создании команд
+    */
     explicit BNDCommandFactory(LibraryData &libraryData) : CommandFactory<CommandClasses>(), libraryData(libraryData){}
 
+    /**
+    * @brief Создает объект команды
+    * @param commandString Строка, представляющая тип создаваемой команды.
+    * @return Общий указатель на объект BaseCommand
+    * @note Переопределено из CommandFactory
+    */
     std::shared_ptr<BaseCommand> getCommand(const std::string &commandString) override {
         return CommandFactory<CommandClasses>::construct(commandString, libraryData);
     }
--- src/commands/BNDCommands.hpp
+++ src/commands/BNDCommands.hpp
@@ -1,7 +1,11 @@
 //
 // Created by maksi on 08.06.2023.
 //
-
+/**
+ * @file BNDCommands.h
+ *
+ * @brief Содержит объявления типов BNDCommandsTuple и BNDCommands.
+ */
 #ifndef BND_PROJ_BNDCOMMANDS_HPP
 #define BND_PROJ_BNDCOMMANDS_HPP
 #include "Init.h"
@@ -16,7 +20,24 @@
 #include "DeleteSection.hpp"
 #include "Load.hpp"
 #include "Save.hpp"
-
+/**
+ * @typedef BNDCommandsTuple
+ *
+ * @brief Тип кортежа, содержащий все доступные команды.
+ *
+ * Кортеж состоит из следующих команд:
+ * - Init
+ * - Print
+ * - Add
+ * - Reorganization
+ * - Delete
+ * - RenameCatalog
+ * - OutSectionAlphabet
+ * - OutFreeSpace
+ * - DeleteSection
+ * - Load
+ * - Save
+ */
 typedef std::tuple<
     Init,
     Print,
@@ -30,6 +51,10 @@ typedef std::tuple<
     Load,
     Save
 > BNDCommandsTuple;
-
+/**
+ * @typedef BNDCommands
+ *
+ * @brief Фабричный класс для выполнения различных команд из кортежа
+ */
 typedef BNDCommandFactory<BNDCommandsTuple> BNDCommands;
 #endif //BND_PROJ_BNDCOMMANDS_HPP
